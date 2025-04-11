85f2818b2cebce73a668f2757eb9eb06188a9b1b
Chegi21 <bilev.jenia@yandex.ru>
2025-04-11 08:24:35 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Поехали!");
 
-        System.out.println("Поехали!");
-
-
         TaskManager manager = new TaskManager();
 
         // Создание двух обычных задач
