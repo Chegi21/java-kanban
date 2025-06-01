bff02f83e0a117e2aae00069b3420d4a6b53c1e1
Chegi21 <bilev.jenia@yandex.ru>
2025-06-01 13:57:03 +0300

    Спринт 7 - исправление Checkstyle

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -14,7 +14,7 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void add(Task task) {
-        if(task != null) {
+        if (task != null) {
             remove(task.getId());
             overwrite(task);
         }
